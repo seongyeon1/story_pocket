133eabea8c38efae13e0b48222685e1045ebd3fc
Hail Roh <rotimeh24k@gmail.com>
2024-10-25 17:56:09 +0900

    add front

--- front/story_teller/my_project/lib/main.dart
+++ front/story_teller/my_project/lib/main.dart
@@ -1,77 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'screens/common/auth/login_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter STT Demo',
+      title: "이야기 들려주세요",
+      debugShowCheckedModeBanner: false,
+
+      // 테마 컬러 설정
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SpeechToTextDemo(),
-    );
-  }
-}
-
-class SpeechToTextDemo extends StatefulWidget {
-  @override
-  _SpeechToTextDemoState createState() => _SpeechToTextDemoState();
-}
-
-class _SpeechToTextDemoState extends State<SpeechToTextDemo> {
-  stt.SpeechToText? _speech; // nullable로 변경
-  bool _isListening = false;
-  String _text = '';
-
-  @override
-  void initState() {
-    super.initState();
-    _speech = stt.SpeechToText(); // 초기화
-  }
-
-  void _listen() async {
-    if (_isListening) {
-      await _speech!.stop(); // null 체크 후 호출
-    } else {
-      await _speech!.initialize(); // null 체크 후 호출
-      await _speech!.listen(onResult: (result) {
-        setState(() {
-          _text = result.recognizedWords;
-        });
-      });
-    }
-    setState(() {
-      _isListening = !_isListening;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Speech to Text Demo'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              _text,
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _listen,
-              child: Text(_isListening ? 'Stop Listening' : 'Start Listening'),
-            ),
-          ],
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.brown,
         ),
+        useMaterial3: true,
       ),
+      home: const LoginScreen(),
     );
   }
 }
