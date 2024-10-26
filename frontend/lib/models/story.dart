f9913b12f94a5aaf289be265eab806d43dd7e8e3
seongyeon1 <ksy974498@gmail.com>
2024-10-27 01:22:23 +0900

    story

---
+++ frontend/lib/models/story.dart
@@ -0,0 +1,14 @@
+// 스토리 객체 정의
+class Story {
+  String title;
+  String author;
+  String story;
+  int numberOfView;
+
+  Story({
+    required this.title,
+    required this.author,
+    required this.story,
+    required this.numberOfView,
+  });
+}
