1430eb76ac5b1254ede64b44753a8f7e12b2b956
Hail Roh <rotimeh24k@gmail.com>
2024-11-16 18:14:12 +0900

    story_meau

--- front/story_teller/my_project/lib/screens/elder/contants/elder_contants_screen.dart
+++ front/story_teller/my_project/lib/screens/elder/contants/elder_contants_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:my_project/models/story.dart';
+import 'package:my_project/data/story_dummy_data.dart';
+import 'package:my_project/screens/elder/contants/widgets/meau_tile.dart';
 
 class ElderContantsScreen extends StatefulWidget {
   const ElderContantsScreen({super.key});
@@ -8,10 +11,28 @@ class ElderContantsScreen extends StatefulWidget {
 }
 
 class _ElderContantsScreenState extends State<ElderContantsScreen> {
+  //동화 데이터 리스트 저장 변수
+  List<Story> stories = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    stories = storyData
+        .map((data) => Story(
+              title: data['title'] as String,
+              author: data['author'] as String,
+              story: data['story'] as String,
+              numberOfView: data['numberOfView'] as int,
+            ))
+        .toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text("동화책 구경하기"),
         actions: [
           IconButton(
             onPressed: () {},
@@ -27,7 +48,19 @@ class _ElderContantsScreenState extends State<ElderContantsScreen> {
           )
         ],
       ),
-      body: const Text("asds"),
+      //동화 리스트
+      body: ListView.separated(
+        itemCount: storyData.length,
+        itemBuilder: (context, index) {
+          final story = stories[index];
+          final order = index;
+          //타일 위젯
+          return buildStoryTile(story, order, context);
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return const Divider();
+        },
+      ),
     );
   }
 }
